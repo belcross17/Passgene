1f67bbd9682c5820c3796f6e5e424a370521d57e
Belcross17 <alfredochabes1@hotmail.com>
2022-06-26 12:03:55 -0400

    create function generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,15 @@
+
 // Assignment code here
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("You just agreed to create a new password");
+
+  return "Generated Password will go here"
+} 
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +22,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
