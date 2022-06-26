f9c23aa9b60f5b0e252179ac3ae181d06356f4d9
Belcross17 <alfredochabes1@hotmail.com>
2022-06-26 13:30:23 -0400

    2nd possible approach to create password

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,26 @@
 
 // Assignment code here
+var characterlength = 8;
+var choiceArr = [];
+
+var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '~','[', ']', '{','}', ':', ';', '<', '>', '/', '?', '`', '\\', '|','+',];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'];
+var uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'];
+var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+
+function generatePassword() {
+  
+}
+
+function getPrompts() {
+  characterlength = parseInt(prompt("how many characters do you want your paassword to be? between 8 - 128"));
+}
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
-  console.log("You just agreed to create a new password");
-
-//prompt password criteria
-
-//choose password length between # to 128
-
-//prompt choose lowercase, uppercase, numbers, and special characters
-
-//validate input and minimum requirements for password
-
-//display password in page
-  return "Generated Password will go here"
-} 
 
 // Write password to the #password input
 function writePassword() {
@@ -32,3 +34,36 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+
+
+
+// console.log("You just agreed to create a new password");
+
+// //alert password criteria
+//   var passCriteria = window.alert('Select which of the following criterias, you would like to include in your password, make sure to include at least one.');
+// //choose password length between # to 128
+//   var passLength = paserInt(window.prompt('Please select the length of your password. Choose a number between 8 and 128'));
+//   console.log(passLength);
+//   if (passLength === "" || passLength === null) {
+//     window.alert("you need to input a valid answer, try again.");
+
+//     return generatePassword();
+
+//   } else if (passLength < 8 || passLength > 128) {
+//     window.alert("Wrong password length, please try again");
+
+//     return generatePassword();
+
+//   } else if (passLength => 8 || passLength == 128) {
+//     window.alert("You chose " + passLength + " as your password length.");
+//   }
+  
+// //prompt choose lowercase, uppercase, numbers, and special characters
+
+// //validate input and minimum requirements for password
+
+// //display password in page
+//   return "Generated Password will go here"
+// } 
