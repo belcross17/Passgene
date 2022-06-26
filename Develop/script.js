f778c6c6b1a39898ab68a4b77eb9151835af120c
Belcross17 <alfredochabes1@hotmail.com>
2022-06-26 12:17:50 -0400

    layout of steps

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,15 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("You just agreed to create a new password");
 
+//prompt password criteria
+
+//choose password length between # to 128
+
+//prompt choose lowercase, uppercase, numbers, and special characters
+
+//validate input and minimum requirements for password
+
+//display password in page
   return "Generated Password will go here"
 } 
 
